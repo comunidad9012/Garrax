520080420405782339567efb065b6723aaa7cb7b
Ramiro <corraramiro02@gmail.com>
2024-09-19 15:30:53 -0300

    implementacion de react

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "mysql+pymysql://root:PROGRAMACION2023@localhost/hotel")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ frontend/src/componentes/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BaseLayout from './BaseLayout';
+
+function Home() {
+  return (
+    <BaseLayout>
+      <h1>Bienvenido a Hotel Garrax</h1>
+      <p>Esta es la página principal.</p>
+    </BaseLayout>
+  );
+}
+
+export default Home;
