adc5081397c9cae5018631ee9f0ae229ac4cfad3
GabiR25 <136388086+GabiR25@users.noreply.github.com>
2024-10-02 05:41:57 -0300

    Delete backend directory

--- backend/app/routes/home.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template, Blueprint
-
-home_bp=Blueprint('home_bp', __name__)
-
-@home_bp.route('/')
-def home():
-    return render_template('home.html')
--- backend/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "mysql+pymysql://root:PROGRAMACION2023@localhost/proyecto1")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
